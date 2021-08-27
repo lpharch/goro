d8a8c6dcabae4d3bcb8ab9cbdb9db5aefd77ff49
Chameleon Cloud User <cc@mm.novalocal>
2021-08-27 21:09:49 +0000

    added RL inference

--- gem5/configs/common/Options.py
+++ gem5/configs/common/Options.py
@@ -146,6 +146,7 @@ def addNoISAOptions(parser):
     parser.add_option("--mode", type="string", default="baseline",
         help="mode of simulation: baseline, multi, RL, random" )
     parser.add_option("--train", action="store_true")
+    parser.add_option("--inference", action="store_true")
     parser.add_option("--num_sample", type="int", default=10)
     parser.add_option("--sample_length", type="int", default=10000000)
     
